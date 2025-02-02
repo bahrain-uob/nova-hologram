b429ab0cee4b3346528b6782b240b3520522eff1
maramesk <maramesk@amazon.com>
2025-02-02 16:48:50 +0300

    adding missing react files

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    // Replace YOUR_API_GATEWAY_URL with the actual API Gateway URL
+    fetch("https://4k4367ikcc.execute-api.us-east-1.amazonaws.com/prod/")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch((err) => console.error("Error fetching data:", err));
+  }, []);
+
+  return (
+    <div>
+      <h1>Welcome to Tawasul.bh</h1>
+      <p>Message from the Backend: {message}</p>
+    </div>
+  );
+}
+
+export default App;
