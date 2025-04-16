4cf2d104cbd1f6474cdcc88842dc8473e7a8f46e
xlvk <fatima77614@gmail.com>
2025-04-16 12:09:54 +0300

    fix(code): fixed the deployment errors.

--- frontend-next/src/app/login/page.js
+++ frontend-next/src/app/login/page.js
@@ -9,7 +9,7 @@ import "./login.css";
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
+  // const [error, setError] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
