06b526e907368e7b5d353215327cc92f6167f41d
mohammedALASAD <alasadmohammed2003@gmail.com>
2025-04-20 10:29:03 +0300

    Update textract(try).py

--- lambda/TextExtraction/textract(try).py
+++ lambda/TextExtraction/textract(try).py
@@ -1,13 +1,29 @@
 import boto3
 textract = boto3. client( 'textract')
-with open ('/Users/mohammedalasad/Desktop/nova-hologram/lambda/TextExtraction/img.jpeg','rb') as img:
+with open ('/Users/mohammedalasad/Desktop/nova-hologram/lambda/TextExtraction/img1.jpeg','rb') as img:
     document_bytes = img.read()
 
 response= textract.detect_document_text(Document = {'Bytes': document_bytes})
+
+print ("printing the text in the imge")
 for item in response[ 'Blocks']:
     if item[ 'BlockType'] == 'LINE':
         print (item['Text'])
 
+
+'''
+print ("printing the Key value pairs") # For the future 
+for item in response[ 'Blocks']:
+    if item[ 'BlockType'] == 'KEY_VALUE_SET':
+        print (item['Text'])    
+
+print ("printing the Key value Tables") # For the future 
+for item in response[ 'Blocks']:
+    if item[ 'BlockType'] == 'TABLE':
+        print (item['Text']) 
+'''
+
 # This code should print (You can do it!) like what is in the (img.jpeg)
         # in real life we will apload more content pages, which will print (extract) More 
-# Mke sure to modify the path of the image in line 3 <3
+        # this code could be 
+# Make sure to modify the path of the image in line 3 <3
